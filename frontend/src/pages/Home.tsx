8dffcd29e9362da10d4e92dba32be869a313ed22
bananabread01 <smriti.20211434@iit.ac.lk>
2025-04-02 04:10:33 +0530

    [fix] responsive for small screen

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,10 +5,10 @@ import homeImage from "@/assets/home-page.jpg";
 
 const Home = () => {
     return (
-        <section className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
+        <section className="min-h-screen flex flex-col md:flex-row items-center justify-center bg-gray-100 dark:bg-gray-900">
             {/* Left Side*/}
-            <div className="w-1/2 flex flex-col items-start px-12">
-                <h1 className="text-6xl font-bold tracking-tight text-gray-900 dark:text-white leading-tight">
+            <div className="w-full md:w-1/2 flex flex-col items-start px-6 md:px-12 mb-8 md:mb-0">
+                <h1 className="text-4xl md:text-6xl font-bold tracking-tight text-gray-900 dark:text-white leading-tight text-center md:text-left">
                     Gallbladder Cancer Detection
                 </h1>
                 <p className="text-lg text-gray-600 dark:text-gray-300 mt-4">
@@ -25,11 +25,11 @@ const Home = () => {
             </div>
 
             {/* Right Side*/}
-            <div className="w-1/2 flex justify-center">
+            <div className="w-full md:w-1/2 flex justify-center px-6">
                 <img 
                     src={homeImage}
                     alt="Ultrasound Illustration" 
-                    className=" rounded-lg shadow-lg mr-20"
+                    className=" rounded-lg shadow-lg max-w-full h-auto mx-auto  md:ml-[-40px]"
                 />
             </div>
         </section>
