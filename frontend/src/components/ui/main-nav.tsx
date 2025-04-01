f2f137060b34693c161044c36326e359fa7c9d68
bananabread01 <smriti.20211434@iit.ac.lk>
2025-04-02 03:34:55 +0530

    [fix] navbar links

--- frontend/src/components/ui/main-nav.tsx
+++ frontend/src/components/ui/main-nav.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils"
-import { useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import { ModeToggle } from "../mode-toggle"
 
 export default function MainNav() {
@@ -8,49 +8,46 @@ export default function MainNav() {
     return (
     <div className="w-full flex items-center justify-between p-4">        
         <div className="flex items-center space-x-6">
-        <a href="/" className="ml-6 mr-6 flex items-center space-x-2">
+        <Link to="/" className="ml-6 mr-6 flex items-center space-x-2">
             <span className="hidden font-bold sm:inline-block">
                 {"GBDetect"}
             </span>
-        </a>
+        </Link>
+
         {/* Center Navigation */}
         <nav className="flex items-center space-x-6 text-sm font-medium">
-            <a
-                href="/"
+            <Link
+                to="/"
                 className={cn(
                     "transition-colors hover:text-foreground/80",
-                    location.pathname === "/home" ? "text-foreground" : "text-foreground/60"
+                    location.pathname === "/" ? "text-foreground" : "text-foreground/60"
                 )}
             >
                 Home
-            </a>
-            {/* <a
-                href="/app"
-                className={cn(
-                    "transition-colors hover:text-foreground/80",
-                    location.pathname === "/app" ? "text-foreground" : "text-foreground/60"
-                )}
-            >
-                App
-            </a> */}
-            <a
-                href="/upload"
+                    
+            </Link>
+
+            <Link
+                to="/upload"
                 className={cn(
                     "transition-colors hover:text-foreground/80",
                     location.pathname === "/upload" ? "text-foreground" : "text-foreground/60"
                 )}
             >
                 Predict
-            </a>
-            <a
-                href="/instructions"
+                    
+            </Link>
+
+            <Link
+                to="/instructions"
                 className={cn(
                     "transition-colors hover:text-foreground/80",
                     location.pathname === "/instructions" ? "text-foreground" : "text-foreground/60"
                 )}
             >
                 Instructions
-            </a>
+                    
+            </Link>
         </nav>
         </div>
         <div className="flex items-center mr-2">
